9ade2f7b0a19881dfc960fd3156dc31e7ccc0764
Arpit Singh <arpitsinghthaku321@gmail.com>
2023-03-25 23:52:24 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './App.css';
 import About from './About'
-import Contact from './Contact'
+// import Contact from './Contact'
 function App() {
-  let data ="Something code of js"
+  // let data ="Use of state"
   return (
     <div className="App">
       <h1>Hello from react</h1>
       <About   Name="Arpit Singh"/>
        
-       <h1>{data}</h1>
-      <Contact/>
+       {/* <h1>{data}</h1> */}
+      {/* <Contact/> */}
     </div>
   );
 }
